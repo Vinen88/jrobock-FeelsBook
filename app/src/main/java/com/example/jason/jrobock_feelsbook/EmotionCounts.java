6d162f60f8419274559bd0eae349b9c6d33c656c
Jason <robockj@mymacewan.ca>
2018-10-04 15:17:31 -0600

    I lied added now

--- app/src/main/java/com/example/jason/jrobock_feelsbook/EmotionCounts.java
+++ app/src/main/java/com/example/jason/jrobock_feelsbook/EmotionCounts.java
@@ -1,4 +1,13 @@
 package com.example.jason.jrobock_feelsbook;
 
 public class EmotionCounts {
+    private static int[] emotionCounts = null;
+    static public int[] getEmotionCounts() {
+        if (emotionCounts == null){
+            emotionCounts =  new int[] {0,0,0,0,0,0};
+
+        }
+        return emotionCounts;
+    }
+
 }
